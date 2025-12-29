3f06485e52ba27f5ea211bdc470538e11c3b4024
Dvniels <kajugadaniels@gmail.com>
2025-12-29 20:23:32 +0200

    Add sign in directory

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+export default function AuthLayout({
+    children,
+}: Readonly<{ children: ReactNode }>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
