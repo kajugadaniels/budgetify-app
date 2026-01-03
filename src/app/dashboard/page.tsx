588d8d775a4fa2398f7d86bda9a67d3754b2687d
kajugadaniels <kajugadaniels@gmail.com>
2026-01-03 15:47:58 +0200

    Protect dashboard and sync user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,16 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { syncUser } from "@/lib/actions/user";
 
 export default async function Dashboard() {
-    const user = await currentUser();
+    const { userId } = auth();
+    if (!userId) {
+        redirect("/");
+    }
 
-    // the best way of syncing => webhooks
     await syncUser();
 
-    // redirect auth user to dashboard
-    if (user) redirect("/dashboard");
-
     return (
-        <div className="min-h-screen bg-background">
-            Dashboard here
-        </div>
+        <div className="min-h-screen bg-background">Dashboard here</div>
     );
 }
